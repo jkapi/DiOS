0afc0de06f915ce2845bbca8967a598e49b9e440
Pedro Samora <psamora@outlook.com>
2016-07-04 22:18:59 -0400

    OS dev meaty-skeleton with my versions of core kernel libc

---
+++ iso.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+set -e
+. ./build.sh
+
+mkdir -p isodir
+mkdir -p isodir/boot
+mkdir -p isodir/boot/grub
+
+cp sysroot/boot/dios.kernel isodir/boot/dios.kernel
+cat > isodir/boot/grub/grub.cfg << EOF
+menuentry "dios" {
+	multiboot /boot/dios.kernel
+}
+EOF
+grub-mkrescue -o dios.iso isodir
